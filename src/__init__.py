68a20e258c4364795e23858eef940cb8212ad0e2
Nceba Mandizvidza <ncebamandizvidza@gmail.com>
2025-09-15 23:57:17 +0200

    Email configurations

--- src/__init__.py
+++ src/__init__.py
@@ -41,12 +41,12 @@ def create_app():
     app.config["SECRET_KEY"] = "hehehehehe"
 
     # Email configuration
-    app.config["MAIL_SERVER"] = "smtp.example.com"
+    app.config["MAIL_SERVER"] = "smtp.ncebamandizvidza.com"
     app.config["MAIL_PORT"] = 587
     app.config["MAIL_USE_TLS"] = True
-    app.config["MAIL_USERNAME"] = "your_username"
-    app.config["MAIL_PASSWORD"] = "your_password"
-    app.config["MAIL_DEFAULT_SENDER"] = "your_email@example.com"
+    app.config["MAIL_USERNAME"] = "dummy"
+    app.config["MAIL_PASSWORD"] = "dummy"
+    app.config["MAIL_DEFAULT_SENDER"] = "me@ncebamandizvidza.com"
 
     # Photo uploads configuration
     app.config["UPLOADED_PHOTOS_DEST"] = path.join(basedir, "static/img")
